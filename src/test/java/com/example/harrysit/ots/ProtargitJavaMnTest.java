14f19a349d7be8547283173e7d0d7d5cd9c54ab5
Magnus Harrysson <ma.harrysson@gmail.com>
2022-05-04 15:58:31 +0000

    Initial commit

---
+++ src/test/java/com/example/harrysit/ots/ProtargitJavaMnTest.java
@@ -0,0 +1,21 @@
+package com.example.harrysit.ots;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class ProtargitJavaMnTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
